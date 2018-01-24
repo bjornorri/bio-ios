fbeed6eceaca4937736cfde2712b5f6db4001df0
Björn Orri Sæmundsson <bjornorri@gmail.com>
2018-01-24 16:31:59 +0100

    File sync

---
+++ bio/Showtimes/ScheduleCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleCell.swift
+//  bio
+//
+//  Created by Bjorn Orri Saemundsson on 17/01/2018.
+//  Copyright © 2018 Bjorn Orri Saemundsson. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
